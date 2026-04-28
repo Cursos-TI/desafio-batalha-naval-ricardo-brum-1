357110350a0d13dd9e5b2bd4c2ad373a6a3b84ee
Ricardo Brum <ricardo-rj86@hotmail.com>
2026-04-28 20:49:37 +0000

    Desafio batalha naval nivel aventureiro concluido

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -6,20 +6,34 @@ int main() {
     // Nível Novato - Posicionamento dos Navios
 
     int tabuleiro[10][10];
-    //int navio1[3] = {3, 3, 3};
-    //int navio2[3] = {3, 3, 3};
-    //char coluna[10] = {'A','B','C','D','E','F','G','H','I','J'};
+    char coluna[10] = {'A','B','C','D','E','F','G','H','I','J'};
     int linha[10] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
 
-    printf("    A  B  C  D  E  F  G  H  I  J\n"); // imprimindo as colunas de A até J
+    // for exclusivo para a impressão da primeira linha com as letras de A até J
+    for(int c = 0; c < 10; c++){
+        if(c == 0){
+            printf("%5c", coluna[c]);
+        } else{
+            printf("%3c", coluna[c]);
+        }
+        
+    }
+    printf("\n");
+
     for(int i = 0; i < 10; i++){
-        printf("%2d  ", linha[i]); // imprimindo as linhas de 1 a 10
+        printf("%2d  ", linha[i]);//imprimindo as linhas de 1 a 10 alinhado na segunda casa decimal
 
         for(int j = 0; j < 10; j++){
 
-            if(i == 1 && j >= 2 && j <=4){ // posição do navio 1
+            if(i == 1 && j >= 2 && j <=4){ // posição do navio 1 horizontal(C2/D2/E2)
                 tabuleiro[i][j] = 3;
-            } else if(j == 7 && i >= 6 && i <= 8){ // posição do navio 2
+            } else if(j == 7 && i >= 6 && i <= 8){ // posição do navio 2 vertical(H7/H8/H9)
+                tabuleiro[i][j] = 3;
+            } else if(i == j && i >= 3 && i <= 5){   // posição do navio 3 diagonal(D4/E5/F6)
+                tabuleiro[i][j] = 3;
+            } else if(i + j == 9 && i >= 6 && i <= 8){   // posição do navio 4 diagonal(D7/C8/B9)
+                tabuleiro[i][j] = 3;
+            } else if(j - i == 5 && i >= 1 && i <= 3){ // posição do navio 5 diagonal(G2/H3/I4)
                 tabuleiro[i][j] = 3;
             } else{ // deu água
                 tabuleiro[i][j] = 0;
